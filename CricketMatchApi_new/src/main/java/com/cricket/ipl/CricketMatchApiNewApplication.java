4a7d7dc7d6c591331e250f9a672eda2dcfd7f065
chayan0104 <chayansamanta8@gmail.com>
2025-02-26 22:16:52 +0530

    new

---
+++ CricketMatchApi_new/src/main/java/com/cricket/ipl/CricketMatchApiNewApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.ipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketMatchApiNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketMatchApiNewApplication.class, args);
+	}
+
+}
