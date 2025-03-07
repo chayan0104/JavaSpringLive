8b1af52195c36a0a3f29d5ab7986cd7042c5f5a8
chayan0104 <chayansamanta8@gmail.com>
2025-03-08 02:24:44 +0530

    Updates

--- RestApidemo/src/main/java/com/demoApi/RestApidemoApplication.java
+++ RestApidemo/src/main/java/com/demoApi/RestApidemoApplication.java
@@ -2,12 +2,18 @@ package com.demoApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class RestApidemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApidemoApplication.class, args);
+//		ApplicationContext context = SpringApplication.run(RestApidemoApplication.class, args);
+
+//		UserController usc = context.getBean(UserController.class);
+
+		System.out.println("api running");
 	}
 
 }
--- RestApidemo/src/main/java/com/demoApi/controller/UserController.java
+++ RestApidemo/src/main/java/com/demoApi/controller/UserController.java
@@ -1,16 +1,52 @@
 package com.demoApi.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-	
+
+//	    http://localhost:7473/api/v1/user/register?name=ram
+//	    protocol+host+ port- http://localhost:7473/
+//		base url-api/v1/user
+//		endpoint url-/register
+//		query-name=ram
+
+	String regex = "^[a-zA-Z]+$";
+
+	// getting the query data with the help of @requestparam
 	@PostMapping("/register")
-	public String userRegister() {
-		return"";
+	public String userRegister(@RequestParam(name = "uname") String userName,
+			@RequestParam(name = "pass") String pass) {
+
+		if (userName == null || userName.trim().isEmpty()) {
+			return "Enter A valid name!";
+		}
+		if (!userName.matches(regex)) {
+			return "Enter A valid name!";
+		} else {
+			return "Hello there, " + userName + " and password is:" + pass;
+		}
 	}
 
+	// Another way direct
+	@PostMapping("/register2")
+	public String userRegister2(@RequestParam String userName, @RequestParam String pass) {
+		return "Hello there, " + userName + " and password is:" + pass;
+	}
+
+	// With the use of map
+	@PostMapping("/register3")
+	public String userRegister3(@RequestParam Map<String, String> params) {
+		String userName = params.get("uname");
+		String pass = params.get("pass");
+		
+		return "Hello there, " + userName + " and password is:" + pass;
+//		return params.entrySet().toString();
+	}
 }
